c9b3c1b647e6cef7e3ac7e200065ecd3e55bca72
josiebeepboop <josette.bitanga@gmail.com>
2022-11-26 20:57:24 -0500

    updated logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -7,16 +7,24 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      40.7, -94.5
+      34.0522,-118.2437
     ],
-    zoom: 4
+    zoom: 14
   });
+// // Add a marker on LA
+//   let marker = L.marker([34.0522,-118.2437]).addTo(map);
+
+// Add a circle marker on LA
+let circle = L.circle([34.0522,-118.2437], {
+  radius: 300,
+color: "black",
+fillcolor: '#ffffa1'}).addTo(map);
 
   // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
