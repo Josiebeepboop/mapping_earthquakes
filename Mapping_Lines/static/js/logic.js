cb63042e9826eeef2e705c19ea6e1bd354413dda
josiebeepboop <josette.bitanga@gmail.com>
2022-11-28 22:02:51 -0500

    updated

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -7,37 +7,32 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      34.05,-118.24
+      37.6214, -122.3790
     ],
-    zoom: 14
+    zoom: 5
   });
-// // Add a marker on LA
-//   let marker = L.marker([34.0522,-118.2437]).addTo(map);
+// Coordinates for each point to be used in the line.
+  let line = [
+  [37.6214, -122.3790],
+  [30.1975,-97.6664],
+  [43.6777,-79.6248],
+  [45.4657,-73.7455],
+  [40.6413,-73.7781]
+];
 
-// // Add a circle marker on LA
-// let circle = L.circle([34.0522,-118.2437], {
-//   radius: 300,
-// color: "black",
-// fillcolor: '#ffffa1'}).addTo(map);
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city
-cityData.forEach(function(city){
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/200000,
-    color: "orange",
-    fillOpacity: 0.5,
-    lineweight: 4})
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "blue",
+  weight: 4,
+  dashArray: "4, 10",
+  opacity: 0.5
+}).addTo(map);
 
   // We create the tile layer that will be the background of our map.
-  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/light-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
